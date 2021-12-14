bf98c40b1689a947e217ee418d776221506fb65a
nkq232 <khanhquanflash@gmail.com>
2021-12-15 00:14:13 +0700

    Final Result

--- Sensor/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import org.json.JSONObject;
-
-public class Main {
-    public static void main(String[] args) {
-        Sensor temperature = new Sensor("24", "8", "localhost");
-        temperature.sendData();
-    }
-}
